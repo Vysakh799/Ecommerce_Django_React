b9f1371b29284752ffd927bd45e86bebd628369b
Vysakh <vysakh7995@gmail.com>
2024-07-30 21:00:37 +0530

    adding style and products

--- Ecommerce_frontend/src/App.jsx
+++ Ecommerce_frontend/src/App.jsx
@@ -3,22 +3,19 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Navbar from './components/Navbar'
+import Home from './components/Home'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-  
 function App(){
     return (
       <BrowserRouter>
         <Routes>
           <Route path='/' element={<Navbar/>}>
-              {/* <Route index element={<Home/>} />
-              <Route path='blogs' element={<Blogs/>} />
-              <Route path='contact' element={<Contact/>} /> */}
+              <Route index element={<Home/>} />
           </Route>
         </Routes>
       </BrowserRouter>
     )
   }
 export default App
-      
 
 
--- Ecommerce_frontend/src/components/home.jsx
+++ Ecommerce_frontend/src/components/home.jsx
@@ -1,15 +1,35 @@
 import React,{useState,useEffect} from "react";
 import axios from 'axios'
 
-const ProductList=()=>{
+const Home=()=>{
     const [product,setProduct]=useState([])
-
     useEffect(()=>{
         axios.get('https://apiecommerce.pythonanywhere.com/api/product/')
-            .then(response=>setTask(response.data))
+            .then(response=>setProduct(response.data))
             .catch(error => console.log(error))
-
     },[])
-    
-
+    return (
+        <div className="container mt-3">
+            {product.map(product=>(
+                <a href="">
+                    {/* <div className="card" style="width:400px">
+                        <img className="card-img-top" src={product.image} alt="Card image" style="width:100%" />
+                        <div className="card-body">
+                            <h4 className="card-title">John Doe</h4>
+                            <p className="card-text">Some example text some example text. John Doe is an architect and engineer</p>
+                            <a href="#" className="btn btn-primary">See Profile</a>
+                        </div>
+                    </div> */}
+                    <div className="card">
+                        <img className="card-img-top" src={product.image} alt="product image" />
+                        <div className="card-body">
+                            <h4 className="card-little">{product.p_name}</h4>
+                            <p></p>
+                        </div>
+                    </div>
+                </a>
+            ))}
+        </div>
+    )
 }
+export default Home
